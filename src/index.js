36e05bdc1c9417c8e25635ace91191df617ea878
Kai Roper-Blackman <kai.roper-blackman@and.digital>
2020-04-20 11:01:22 +0100

    added 404 route

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ app.get("/status", routeStatus);
 app.get("/messages", routeGetAll);
 app.post("/messages", routeCreateMessage);
 
+app.get("*", function(req, res){
+  res.status(404).send("Invalid route")
+})
 app.listen(port, () => {
   console.log('Chat api started on: ' + port);
 });
